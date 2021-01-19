b29a2fa1382749579c2fb43a2267d0a6dd0ffa48
henrydapp <hotlotdapp@gmail.com>
2021-01-19 15:02:31 +0000

    Update endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   // rpc: 'https://data-seed-prebsc-2-s1.binance.org:8545',
   // chainId: 97,
-  rpc: 'https://bsc-dataseed.binance.org',
+  rpc: 'https://bsc-private-dataseed1.nariox.org',
   // rpc: 'https://bsc-dataseed1.binance.org',
   chainId: 56,
   api: ''
